937ef1776fcc412661cb224516ccb5e8d5558491
Hardeep Kaur <hardeepkaur85226@gmail.com>
2026-07-11 14:42:24 +0530

    all pages responsiveness home page and all services

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         dangerouslySetInnerHTML={{ __html: LEGACY_SERVICES_HASH_REDIRECT }}
       />
       <HomeSchemaScript />
-      <link rel="stylesheet" href="/css/home.css?v=20250711o" />
+      <link rel="stylesheet" href="/css/home.css?v=20250711u" />
       <link
         rel="preload"
         href="/assets/images/hero-home-cover.webp"
