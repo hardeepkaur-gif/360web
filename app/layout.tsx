f3aee704c1f11707975a142f235ab0e2f60a5b83
Hardeep Kaur <hardeep.kaur@tekcroft.com>
2026-07-25 10:28:53 +0530

    header and body tab added code

--- app/layout.tsx
+++ app/layout.tsx
@@ -159,6 +159,19 @@ export default function RootLayout({
       className={`${inter.variable} ${sora.variable}`}
     >
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PF74BC4M');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <style dangerouslySetInnerHTML={{ __html: INLINE_CSS }} />
         <Script
           id="deferred-styles-loader"
@@ -167,6 +180,16 @@ export default function RootLayout({
         />
       </head>
       <body suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PF74BC4M"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SiteSchemaScript />
         <GeoLayerScript />
         {process.env.NODE_ENV === "production" ? (
