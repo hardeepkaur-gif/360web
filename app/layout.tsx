a069cc6f92d5d47983e76c6d575d253173add2af
Hardeep Kaur <hardeep.kaur@tekcroft.com>
2026-09-10 16:11:05 +0530

    meta pixel code add

--- app/layout.tsx
+++ app/layout.tsx
@@ -180,6 +180,24 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {/* End Google Tag Manager */}
+        {/* Meta Pixel Code */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init','2397962760612638');
+fbq('track','PageView');`,
+          }}
+        />
+        {/* End Meta Pixel Code */}
         <style dangerouslySetInnerHTML={{ __html: INLINE_CSS }} />
         <Script
           id="deferred-styles-loader"
@@ -198,6 +216,17 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+        {/* Meta Pixel (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2397962760612638&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel (noscript) */}
         <SiteSchemaScript />
         <GeoLayerScript />
         {process.env.NODE_ENV === "production" ? (
